59608eee8c9b23553beaf828b436e2b8719676ba
jcsalayo2 <116143080+jcsalayo2@users.noreply.github.com>
2023-10-07 01:46:42 +0800

    initial

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+import 'package:invite/services/attend_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Imbitasyon',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade200),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Handaan'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ConfettiController confettiController = ConfettiController(
+    duration: const Duration(seconds: 5),
+  );
+
+  TextEditingController pangalan = TextEditingController();
+  TextEditingController pagkain = TextEditingController();
+  TextEditingController mensahe = TextEditingController();
+
+  bool successState = false;
+  bool loadingState = false;
+
+  @override
+  void initState() {
+    super.initState();
+    confettiController.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('home_background.jpg'),
+                fit: BoxFit.fitHeight),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(width: 2),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(50))),
+            width: MediaQuery.of(context).size.height * 0.8,
+            height: MediaQuery.of(context).size.height,
+            margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.35,
+                left: 20,
+                right: 20),
+            child: SingleChildScrollView(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 50, right: 50, top: 50),
+                  child: Text(
+                    'Kapag natanggap mo ang imbitasyon na ito, ikaw ay inaanyayahan sa aking handaan. Pakiusap, ilagay '
+                    'ang iyong pangalan sa ibaba, isulat kung anong pagkain ang nais mong kainin, at kung may regalo ka '
+                    'para sa akin, isama ito sa iyong mensahe. Pindutin ang "Ako Ay Makakapunta" pagkatapos.',
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 50, right: 50, top: 20),
+                  child: Text('Saan : Sico 2.0\nKailan : Oct 22'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: TextField(
+                    onChanged: (value) => setState(() {}),
+                    controller: pangalan,
+                    decoration: const InputDecoration(hintText: 'Pangalan'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: TextField(
+                    onChanged: (value) => setState(() {}),
+                    controller: pagkain,
+                    decoration:
+                        const InputDecoration(hintText: 'Gustong Kainin'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50, right: 50),
+                  child: TextField(
+                    onChanged: (value) => setState(() {}),
+                    controller: mensahe,
+                    decoration: const InputDecoration(hintText: 'Mensahe'),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 50, right: 50, top: 20),
+                    child: ElevatedButton(
+                        onPressed: pangalan.text == '' ||
+                                pagkain.text == '' ||
+                                mensahe.text == '' ||
+                                loadingState
+                            ? null
+                            : () async {
+                                setState(() {
+                                  loadingState = true;
+                                });
+                                var success = await BunbuyVoucherService()
+                                    .addAttendFirebase(pangalan.text,
+                                        pagkain.text, mensahe.text);
+
+                                if (success) {
+                                  setState(() {
+                                    pangalan.text = '';
+                                    pagkain.text = '';
+                                    mensahe.text = '';
+                                    loadingState = false;
+                                  });
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text("Success"),
+                                  ));
+                                } else {
+                                  setState(() {
+                                    loadingState = false;
+                                  });
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text("Something Went Wrong"),
+                                  ));
+                                }
+                              },
+                        child: Text('Ako Ay Makakapunta'))),
+              ],
+            )),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: ConfettiWidget(
+            gravity: 0.01,
+            emissionFrequency: 0.04,
+            blastDirectionality: BlastDirectionality.directional,
+            blastDirection: 0,
+            minimumSize: const Size(1, 1),
+            maximumSize: const Size(5, 30),
+            shouldLoop: true,
+            numberOfParticles: 5,
+            confettiController: confettiController,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: ConfettiWidget(
+            gravity: 0.01,
+            emissionFrequency: 0.04,
+            blastDirectionality: BlastDirectionality.directional,
+            blastDirection: 3.1415,
+            minimumSize: const Size(1, 1),
+            maximumSize: const Size(5, 30),
+            shouldLoop: true,
+            numberOfParticles: 5,
+            confettiController: confettiController,
+          ),
+        ),
+      ],
+    ));
+  }
+}
---
+++ lib/services/attend_service.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class BunbuyVoucherService {
+  final CollectionReference attend =
+      FirebaseFirestore.instance.collection('attend');
+
+  Future<bool> addAttend(
+      String pangalan, String pagkain, String mensahe) async {
+    try {
+      final response = await http.post(
+        Uri.parse('https://wt4hpsdd-5000.asse.devtunnels.ms/api/Invite'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(
+          <String, Object>{
+            "Name": pangalan,
+            "Food": pagkain,
+            "Comment": mensahe,
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (ex) {
+      return false;
+    }
+
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    // throw Exception('Failed to load Vouchers');
+  }
+
+  Future<bool> addAttendFirebase(
+      String pangalan, String pagkain, String mensahe) async {
+    return await attend.add({
+      "Name": pangalan,
+      "Food": pagkain,
+      "Message": mensahe,
+      "DateTime": DateTime.now()
+    }).then((value) {
+      return true;
+    }).catchError((error) {
+      return false;
+    });
+  }
+}
